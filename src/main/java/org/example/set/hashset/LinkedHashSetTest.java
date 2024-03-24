922a1dc599d57cf9529a273651c29c3672d65e8d
PARKCY\Park <mangoperry2015@gmail.com>
2024-03-24 19:56:03 +0900

    [Collections] LinkedHashSet Study

---
+++ src/main/java/org/example/set/hashset/LinkedHashSetTest.java
@@ -0,0 +1,27 @@
+package org.example.set.hashset;
+
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class LinkedHashSetTest {
+
+    public static void main(String[] args) {
+        /** LinkedHashSet */
+        LinkedHashSet<String> hset = new LinkedHashSet<>();
+
+        hset.add("java");
+        hset.add("oracle");
+        hset.add("jdbc");
+        hset.add("html");
+        hset.add("css");
+
+        System.out.println("hset : " + hset);
+
+        /** 생성된 LinkedHashSet를 가지고 TreeSet으로 객체를 생성하면
+         * 같은 타입의 객체를 자동으로 비교하여 오름차순 정렬됨 */
+        TreeSet<String> tset = new TreeSet<>(hset);
+        System.out.println("hset : " + tset);
+
+    }
+
+}
