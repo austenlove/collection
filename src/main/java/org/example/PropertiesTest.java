692a8bc1004aa03530ddbf125f5eb30a8daf0662
PARKCY\Park <mangoperry2015@gmail.com>
2024-05-08 23:59:33 +0900

    [Map] Properties Study

--- src/main/java/org/example/PropertiesTest.java
+++ src/main/java/org/example/PropertiesTest.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 
 public class PropertiesTest {
 
-    /** Hashtable을 상속받아 구현한 구현체로,
+    /** Properties : Hashtable을 상속받아 구현한 구현체
      * (Object, Object)의 형태로 저장하는 해시 테이블과 달리
      * (String, String) 형태로 저장
      *
@@ -13,12 +13,17 @@ public class PropertiesTest {
      */
 
     public static void main(String[] args) {
+
         // 설정 파일 값을 읽어서 어플리케이션에 적용 시 사용
         Properties prop = new Properties();
 
         prop.setProperty("driver", "oracle.jdbc.driver.OracleDriver");
+        prop.setProperty("url", "jdbc:oracle:thin:@127.0.0.1:1521:xe");
+        prop.setProperty("user", "student");
+        prop.setProperty("password", "student");
 
+        System.out.println(prop);
+
+//        try {prop} catch () {}
     }
-
-
 }
